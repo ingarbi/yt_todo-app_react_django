805d8c2a0efcb6fb28f52de0bb37588e4cdc7d8e
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-14 17:17:14 +0300

    #2 serializer

--- backend_api/core/models.py
+++ backend_api/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField(max_length=200)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
--- backend_api/core/views.py
+++ backend_api/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+
+from .models import Todo
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def todos_list(request):
+    todos = Todo.objects.all()
+    serializer = TodoSerializer()
