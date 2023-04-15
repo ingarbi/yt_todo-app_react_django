bbabedc6bc744aac853afe9dc859ac32c7b6826e
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-15 22:57:22 +0300

    #4 plus icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 function App() {
+
+  const addTodoHandler = () =>{
+    console.log("click");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-900 text-white h-screen relative">
+      <div className="flex flex-col w-full p-10">
+        <h1 className="text-5xl text-center pb-5">Todo App</h1>
+        <div className="flex items-center justify-between bg-slate-700 rounded-xl px-4">
+          <input
+            type="text"
+            className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+          />
+          <i onClick={addTodoHandler}>
+            <PlusIcon className="h-5 w-5 cursor-pointer hover:opacity-70" />
+          </i>
+        </div>
+      </div>
     </div>
   );
 }
